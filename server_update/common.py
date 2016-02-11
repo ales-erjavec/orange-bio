1bbdea5c394681412ac6a571084ad3a4c0b720c9
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2016-02-11 13:54:33 +0100

    updateGO: Update the GO server files update script

--- server_update/common.py
+++ server_update/common.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 import Orange
 import Orange.utils.serverfiles as serverfiles
 import Orange.utils.environ as environ
@@ -16,8 +18,8 @@ parser.add_option("-m", "--mailto", help="e-mail the results to EMAIL", metavar=
 option, args = parser.parse_args()
 
 if not option.user or not option.password:
-    print "Pass -u username -p password!"
+    print("Pass -u username -p password!")
     sys.exit(1)
 
 sf_server = serverfiles.ServerFiles(option.user, option.password)
-sf_local = Orange.utils.serverfiles
+sf_local = serverfiles
--- server_update/updateGO.py
+++ server_update/updateGO.py
@@ -1,30 +1,29 @@
 ##!interval=7
 ##!contact=ales.erjavec@fri.uni-lj.si
+from __future__ import print_function
 
 import urllib2
 import re
 import cPickle
-import tarfile
+import gzip
+import io
 
 from datetime import datetime
 from collections import defaultdict
 
 from common import *
 
-from Orange.bio import obiGO, obiTaxonomy, obiGene
+from orangecontrib.bio import go, taxonomy, gene, utils
 
 
 tmp_path = os.path.join(environ.buffer_dir, "tmp_GO")
-try:
-    os.makedirs(tmp_path)
-except Exception:
-    pass
-
-uncompressedSize = lambda filename: sum(info.size for info in tarfile.open(filename).getmembers())
+utils.makedirs(tmp_path, exist_ok=True)
 
 DATE_FMT_1 = "%Y-%m-%d %H:%M:%S.%f"
 DATE_FMT_2 = "%Y-%m-%d %H:%M:%S"
 
+
+fileslist = sf_server.listfiles("GO")
 
 def info_date_time_parse(time):
     """
@@ -55,8 +54,12 @@ def list_available_organisms():
 
 
 def sf_org_mtime(org_code):
-    info = sf_server.info("GO", "gene_association.{}.tar.gz".format(org_code))
-    return info_date_time_parse(info["datetime"])
+    fname = "gene_association.{}".format(org_code)
+    if fname in fileslist:
+        info = sf_server.info("GO", fname)
+        return info_date_time_parse(info["datetime"])
+    else:
+        return datetime.fromtimestamp(0)
 
 
 def web_org_mtime(org_code):
@@ -66,8 +69,12 @@ def web_org_mtime(org_code):
 
 
 def sf_ontology_mtime():
-    info = sf_server.info("GO", "gene_ontology_edit.obo.tar.gz")
-    return info_date_time_parse(info["datetime"])
+    fname = "gene_ontology_edit.obo"
+    if fname in fileslist:
+        info = sf_server.info("GO", fname)
+        return info_date_time_parse(info["datetime"])
+    else:
+        return datetime.fromtimestamp(0)
 
 
 def web_ontology_mtime():
@@ -75,36 +82,63 @@ def web_ontology_mtime():
         "http://www.geneontology.org/ontology/gene_ontology.obo")
 
 
-if web_ontology_mtime() > sf_ontology_mtime():
-    print "donwloading ontology"
-    filename = os.path.join(tmp_path, "gene_ontology_edit.obo.tar.gz")
-    obiGO.Ontology.DownloadOntology(filename)
+def gzip_file(srcfname, dstfname=None):
+    if dstfname is None:
+        dstfname = srcfname + ".gz"
 
-    ##load the ontology to test it
-    o = obiGO.Ontology(filename)
+    with open(srcfname, "rb") as file:
+        with gzip.open(dstfname, "wb") as gzfile:
+            utils.copyfileobj(file, gzfile)
+
+
+def gz_uncompressed_size(filename):
+    """
+    WARNING: The reported size is modulo 2 ** 32 i.e. only correct for files
+    smaller than ~4GB
+    """
+    import struct
+    with open(filename, "rb") as f:
+        # the size (modulo 2 ** 32) is encoded in the last 4 bytes
+        # little-endian order (RFC 1952)
+        f.seek(-4, io.SEEK_END)
+        size, = struct.unpack("<I", f.read(4))
+        return size
+
+
+if web_ontology_mtime() > sf_ontology_mtime():
+    print("donwloading ontology")
+    filename = os.path.join(tmp_path, "gene_ontology_edit.obo")
+
+    go.Ontology.download(filename)
+
+    # load the ontology to test it
+    o = go.Ontology(filename)
     del o
+    size = os.stat(filename).st_size
+    # gzip it
+    gzip_file(filename)
+
     ##upload the ontology
-    print "Uploading gene_ontology_edit.obo.tar.gz"
+    print("Uploading gene_ontology_edit.obo")
     sf_server.upload(
-        "GO", "gene_ontology_edit.obo.tar.gz", filename,
+        "GO", "gene_ontology_edit.obo", filename + ".gz",
         title="Gene Ontology (GO)",
-        tags=["gene", "ontology", "GO", "essential",
-              "#uncompressed:%i" % uncompressedSize(filename),
-              "#version:%i" % obiGO.Ontology.version]
+        tags=["gene", "ontology", "GO", "essential", "#compression:gz",
+              "#uncompressed:%i" % size,
+              "#version:%i" % go.Ontology.version]
     )
-    sf_server.unprotect("GO", "gene_ontology_edit.obo.tar.gz")
+    sf_server.unprotect("GO", "gene_ontology_edit.obo")
 
 
 orgMap = {"352472": "44689", "562": "83333", "3055": None,
           "7955": None, "11103": None, "2104": None, "4754":
           None, "31033": None, "8355": None, "4577": None}
 
-commonOrgs = dict([(obiGO.from_taxid(id), id)
-                   for id in obiTaxonomy.common_taxids()
-                   if obiGO.from_taxid(id) != None])
+commonOrgs = dict([(go.from_taxid(id), id)
+                   for id in taxonomy.common_taxids()
+                   if go.from_taxid(id) != None])
 
-essentialOrgs = [obiGO.from_taxid(id)
-                 for id in obiTaxonomy.essential_taxids()]
+essentialOrgs = [go.from_taxid(id) for id in taxonomy.essential_taxids()]
 
 exclude = ["goa_uniprot", "goa_pdb", "GeneDB_tsetse", "reactome",
            "goa_zebrafish", "goa_rat", "goa_mouse"]
@@ -117,58 +151,58 @@ for org in list_available_organisms():
     if org in exclude or org not in commonOrgs:
         continue
 
-    print "Query", org
+    print("Query", org)
 
     if 1 and web_org_mtime(org) <= sf_org_mtime(org):
         # Skip update
         continue
 
-    print "Updating", org
+    print("Updating", org)
 
-    filename = os.path.join(tmp_path, "gene_association." + org + ".tar.gz")
-    obiGO.Annotations.DownloadAnnotations(org, filename)
-
-    ## Load the annotations to test them and collect all taxon ids from them
-    print filename
-    a = obiGO.Annotations(filename, genematcher=obiGene.GMDirect())
+    filename = os.path.join(tmp_path, "gene_association.{}.gz".format(org))
+    go.Annotations.download(org, filename)
+    # Load the annotations to test them and collect all taxon ids from them
+    print(filename)
+    a = go.Annotations(filename, genematcher=gene.GMDirect())
     taxons = set([ann.Taxon for ann in a.annotations])
-    ## exclude taxons with cardinality 2
+    # exclude taxons with cardinality 2
     taxons = [tax for tax in taxons if "|" not in tax]
     for tax in taxons:
         taxid = tax.split(":", 1)[-1]
         updatedTaxonomy[taxid].add(org)
     del a
 
-    orgName = obiTaxonomy.name(commonOrgs[org])
-    taxid = obiTaxonomy.taxname_to_taxid(orgName)
+    orgName = taxonomy.name(commonOrgs[org])
+    taxid = taxonomy.taxname_to_taxid(orgName)
 
-    print "Uploading", "gene_association." + org + ".tar.gz"
+    print("Uploading", "gene_association.{}.gz".format(org))
     sf_server.upload(
-        "GO", "gene_association." + org + ".tar.gz", filename,
+        "GO", "gene_association.{}".format(org), filename,
         title="GO Annotations for " + orgName,
         tags=["gene", "annotation", "ontology", "GO", orgName,
-              "#uncompressed:%i" % uncompressedSize(filename),
+              "#uncompressed:%i" % gz_uncompressed_size(filename),
               "#organism:" + orgName,
-              "#version:%i" % obiGO.Annotations.version] +
+              "#version:%i" % go.Annotations.version] +
              (["essential"] if org in essentialOrgs else []) +
-             obiTaxonomy.shortname(taxid)
+             taxonomy.shortname(taxid)
     )
-    sf_server.unprotect("GO", "gene_association." + org + ".tar.gz")
+    sf_server.unprotect("GO", "gene_association.{}".format(org))
 
 try:
     tax = cPickle.load(open(sf_local.localpath_download("GO", "taxonomy.pickle"), "rb"))
 except Exception:
     tax = {}
 
-## Upload taxonomy if any differences in the updated taxonomy
+# Upload taxonomy if any differences in the updated taxonomy
 if any(tax.get(key, set()) != updatedTaxonomy.get(key, set())
        for key in set(updatedTaxonomy)):
     tax.update(updatedTaxonomy)
-    cPickle.dump(tax, open(os.path.join(tmp_path, "taxonomy.pickle"), "wb"))
-    print "Uploading", "taxonomy.pickle"
+    cPickle.dump(tax, open(os.path.join(tmp_path, "taxonomy.pickle"), "wb"),
+                 protocol=0)
+    print("Uploading", "taxonomy.pickle")
     sf_server.upload(
         "GO", "taxonomy.pickle", os.path.join(tmp_path, "taxonomy.pickle"),
         title="GO taxon IDs",
         tags=["GO", "taxon", "organism", "essential",
-              "#version:%i" % obiGO.Taxonomy.version])
+              "#version:%i" % go.Taxonomy.version])
     sf_server.unprotect("GO", "taxonomy.pickle")
